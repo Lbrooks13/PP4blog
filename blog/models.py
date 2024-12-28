4b193efc43e5877560feb99caca69f58b22522cd
Liam Brooks <lbrooks1308@gmail.com>
2024-12-28 20:18:47 +0000

    bug fix

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
-    likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+    # likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
 
     class Meta:
         ordering = ["-created_on"]
@@ -27,8 +27,8 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
-    def total_likes(self):
-        return self.likes.count()    
+    # def total_likes(self):
+    #     return self.likes.count()    
 
 
 class Comment(models.Model):
