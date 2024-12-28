914ec89c7dbd24425ab8441b6bdad17680ea517d
Liam Brooks <lbrooks1308@gmail.com>
2024-12-28 18:01:32 +0000

    Bug fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-!wz*n2o)k=(-!x7ie@f4w!o*huay()d)=n_z&735l6s_&^$yy+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
